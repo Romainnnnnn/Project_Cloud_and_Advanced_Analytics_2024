da76c476bfbdb10b9393214f7780611ed8c812de
Romain Hovius <84710595+Romainnnnnn@users.noreply.github.com>
2024-05-23 11:07:44 +0200

    Historical Data almost done

--- UI/pages/historical_data.py
+++ UI/pages/historical_data.py
@@ -20,33 +20,75 @@ st.title('Historical Data')
 
 if 'all_records' not in st.session_state:
     st.session_state['all_records'] = get_data_from_flask('all_records')
-    
+
 df = pd.DataFrame(st.session_state['all_records'])
 
 
 df['date'] = df['date'].apply(format_date)
 
+df['date_time'] = df['date'] + ' ,' + df['time']
 
 st.subheader("Indoor Conditions Over Time")
-fig, ax = plt.subplots()
-ax.plot(df['time'], df['indoor_temp'], label='Indoor Temp (°C)', color='blue')
-ax.plot(df['time'], df['indoor_humidity'], label='Indoor Humidity (%)', color='green')
-ax.plot(df['time'], df['indoor_co2'], label='Air Quality', color='red')
-ax.set_xlabel('Time')
-ax.set_ylabel('Values')
-ax.legend()
-plt.xticks(rotation=45)
-plt.tight_layout()
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric(label="Average Temperature (°C)", value=round(df['indoor_temp'].mean(), 2))
+    fig, ax = plt.subplots()
+    ax.plot(df['date_time'], df['indoor_temp'], label='Indoor Temp (°C)', color='blue')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Values')
+    interval = len(df) // 7
+    ax.set_xticks(df['date_time'][::interval])
+    ax.set_xticklabels(df['date_time'][::interval], rotation=45)
+    ax.legend()
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    st.pyplot(fig)
 
-st.pyplot(fig)
+with col2:
+    st.metric(label="Average Indoor Humidity (%)", value=round(df['indoor_humidity'].mean(), 2))
+    fig, ax = plt.subplots()
+    ax.plot(df['date_time'], df['indoor_humidity'], label='Indoor Humidity (%)', color='green')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Values')
+    interval = len(df) // 7
+    ax.set_xticks(df['date_time'][::interval])
+    ax.set_xticklabels(df['date_time'][::interval], rotation=45)
+    ax.legend()
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    st.pyplot(fig)
+
+with col3:
+    st.metric(label="Average Indoor Co2 (ppm)", value=round(df['indoor_co2'].mean(), 2))
+    fig, ax = plt.subplots()
+    ax.plot(df['date_time'], df['indoor_co2'], label='Indoor C02 (ppm)', color='red')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Values')
+    interval = len(df) // 7
+    ax.set_xticks(df['date_time'][::interval])
+    ax.set_xticklabels(df['date_time'][::interval], rotation=45)
+    ax.legend()
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    st.pyplot(fig)
+
+st.markdown("***")
 
 st.subheader("Outdoor Conditions Over Time")
 fig2, ax2 = plt.subplots()
-ax2.plot(df['time'], df['outdoor_temp'], label='Outdoor Temp (°C)', color='orange')
+ax2.plot(df['date_time'], df['outdoor_temp'], label='Outdoor Temp (°C)', color='orange')
 ax2.set_xlabel('Time')
 ax2.set_ylabel('Temperature (°C)')
+interval = len(df) // 7
+ax2.set_xticks(df['date_time'][::interval])
+ax2.set_xticklabels(df['date_time'][::interval], rotation=45)
 ax2.legend()
 plt.xticks(rotation=45)
 plt.tight_layout()
 
 st.pyplot(fig2)
+
+
+st.markdown("***")
+
+st.dataframe(df)
