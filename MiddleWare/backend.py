359845015232890e65a46d0c90c7298564b52e47
Romain Hovius <84710595+Romainnnnnn@users.noreply.github.com>
2024-05-25 16:33:00 +0200

    openAI

--- MiddleWare/backend.py
+++ MiddleWare/backend.py
@@ -5,6 +5,7 @@ from google.cloud.exceptions import GoogleCloudError
 import requests
 from dotenv import load_dotenv
 from google.cloud import texttospeech
+from openai import OpenAI
 
 load_dotenv()
 
@@ -12,10 +13,14 @@ PROJECT_NAME = os.getenv('PROJECT_NAME')
 key_path = os.getenv('KEY_PATH')
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
 
+CHAT_GPT_API_KEY = 'sk-proj-eRS6U2UPpxAp9JwojPRRT3BlbkFJ8eYMfqQ51liBUnU9BIDS'
+
 client = bigquery.Client(project=PROJECT_NAME)
 client_2 = texttospeech.TextToSpeechClient()
+client_OpenAI = OpenAI()
 API_KEY = os.getenv('API_KEY')
-LOCATION = os.getenv('LOCATION') # 'Lausanne,CH'
+LOCATION = os.getenv('LOCATION')  # 'Lausanne,CH'
+
 
 def get_weather_forecast(api_key, location):
     base_url = "https://api.openweathermap.org/data/2.5/forecast"
@@ -26,6 +31,7 @@ def get_weather_forecast(api_key, location):
     }
     response = requests.get(base_url, params=params)
     return response.json()
+
 
 app = Flask(__name__)
 
@@ -51,8 +57,11 @@ def index():
     return endpoints
 
 
-@app.route('/post/<date>/<time>/<indoor_temp>/<indoor_humidity>/<outdoor_temp>/<outdoor_humidity>/<outdoor_wheather>/<outdoor_windspeed>/<detector_status>/<indoor_co2>')
-def post(date, time, indoor_temp, indoor_humidity, outdoor_temp, outdoor_humidity, outdoor_wheather, outdoor_windspeed, detector_status, indoor_co2):
+@app.route(
+    '/post/<date>/<time>/<indoor_temp>/<indoor_humidity>/<outdoor_temp>/<outdoor_humidity>/<outdoor_wheather'
+    '>/<outdoor_windspeed>/<detector_status>/<indoor_co2>')
+def post(date, time, indoor_temp, indoor_humidity, outdoor_temp, outdoor_humidity, outdoor_wheather, outdoor_windspeed,
+         detector_status, indoor_co2):
     try:
         print("Received data")
         # Convert data types as needed
@@ -78,12 +87,13 @@ def post(date, time, indoor_temp, indoor_humidity, outdoor_temp, outdoor_humidit
             "status": "error",
             "message": str(e)
         }), 500
-    
+
 
 @app.route('/forecast')
 def forecast():
     forecast_data = get_weather_forecast(API_KEY, LOCATION)
     return jsonify(forecast_data)
+
 
 @app.route('/outdoor_temp')
 def outdoor_temp():
@@ -93,6 +103,7 @@ def outdoor_temp():
     current_temp = current_weather['main']['temp']
     return jsonify(current_temp)
 
+
 @app.route('/outdoor_humidity')
 def outdoor_humidity():
     print("got request")
@@ -100,6 +111,7 @@ def outdoor_humidity():
     current_weather = forecast_data['list'][0]
     current_humidity = current_weather['main']['humidity']
     return jsonify(current_humidity)
+
 
 @app.route('/outdoor_windspeed')
 def outdoor_windspeed():
@@ -109,13 +121,15 @@ def outdoor_windspeed():
     current_wind_speed = current_weather['wind']['speed']
     return jsonify(current_wind_speed)
 
-@app.route('/outdoor_weather') 
+
+@app.route('/outdoor_weather')
 def outdoor_weather():
     print("got request")
     forecast_data = get_weather_forecast(API_KEY, LOCATION)
     current_weather = forecast_data['list'][0]
     current_weather = current_weather['weather'][0]['description']
-    return jsonify(current_weather)                           
+    return jsonify(current_weather)
+
 
 @app.route('/get_icon/<forecast>')
 def get_icon(forecast):
@@ -125,7 +139,7 @@ def get_icon(forecast):
     print(f"{base_url}/{icon_url}{end_url}")
     return jsonify(f"{base_url}/{icon_url}{end_url}")
 
-    
+
 @app.route('/time')
 def time_date():
     from datetime import datetime
@@ -133,12 +147,14 @@ def time_date():
     current_time = now.strftime("%H:%M:%S")
     return jsonify(current_time)
 
+
 @app.route('/date')
-def date(): 
+def date():
     from datetime import datetime
     now = datetime.now()
     current_date = now.strftime("%Y-%m-%d")
     return jsonify(current_date)
+
 
 @app.route('/last_record')
 def last_record():
@@ -165,7 +181,7 @@ def last_record():
             "status": "error",
             "message": str(e)
         }), 500
-    
+
 
 @app.route('/all_records')
 def all_records():
@@ -183,16 +199,16 @@ def all_records():
             "status": "error",
             "message": str(e)
         }), 500
-    
+
 
 @app.route('/text_to_speech/<text2>')
 def text_to_speech(text2):
-    synthesis_input = texttospeech.SynthesisInput(text = text2)
+    synthesis_input = texttospeech.SynthesisInput(text=text2)
 
     voice = texttospeech.VoiceSelectionParams(
-        language_code="en-US", 
+        language_code="en-US",
         ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-        )
+    )
 
     audio_config = texttospeech.AudioConfig(
         audio_encoding=texttospeech.AudioEncoding.LINEAR16
@@ -202,6 +218,19 @@ def text_to_speech(text2):
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
     return response.audio_content
+
+
+@app.route('/get_text')
+def get_text():
+    response = client_OpenAI.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        response_format={"type": "json_object"},
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+            {"role": "user", "content": "Who won the world series in 2020?"}
+        ]
+    )
+    return jsonify(response.choices[0].message.content)
 
 
 if __name__ == '__main__':
