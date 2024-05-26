4b3e4c32942ff420b90e176d03b70fe011876c7d
Romain Hovius <84710595+Romainnnnnn@users.noreply.github.com>
2024-05-26 22:40:02 +0200

    tuto

--- Tutorial/pages/uiflow.py
+++ Tutorial/pages/uiflow.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 
 st.title('M5 Stack Setup')
+st.write("##### Copy paste the code in the m5stack folder to the M5 Stack IDE")
 st.write('##### Change the M5 Stack wifi settings and add your local or deployed backend URL')
 image = st.image('data/setup.png', caption='https://flow.m5stack.com/', width=500)
 
@@ -13,6 +14,8 @@ st.write('###### TVOC : port blue : 14/13')
 st.write('###### PIR : port black : 36/26')
 
 st.write('##### Download the firmware onto the M5 Stack')
+
+st.write('###### Click on the download button and wait for the firmware to be downloaded')
 image = st.image('data/dl.png', caption='https://flow.m5stack.com/', width=500)
 
 if st.button('Next Page'):
